fc1dc36317f35ccabdfc9fe86fd09f889089080e
Kavalon Wong <klovelovely@live.com>
2016-04-15 10:31:04 +0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inviteOperationManagmentSystemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inviteOperationManagmentSystemApp
+ */
+angular.module('inviteOperationManagmentSystemApp')
+  .controller('MainCtrl', function () {
+    this.asdf = 'kavalon :3';
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
