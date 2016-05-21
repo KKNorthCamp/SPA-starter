9e2d6363f65c54283ff93734899c536131fe7247
Kavalon Wong <klovelovely@live.com>
2016-05-21 15:44:13 +0800

    暂时移除不需要的 grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
         src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         ignorePath: /(\.\.\/){1,2}bower_components\//
       }
-    }, 
+    },
 
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
@@ -499,7 +499,7 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
+    /*'cdnify',*/
     'cssmin',
     'uglify',
     'filerev',
@@ -510,7 +510,7 @@ module.exports = function (grunt) {
   grunt.registerTask('default', [
     'newer:jshint',
     'newer:jscs',
-    'test',
+    /*'test',*/
     'build'
   ]);
 };
